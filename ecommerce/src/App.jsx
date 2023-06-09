09e96cb057a454168351e33578b52557bf25141a
dkumar1402 <136038211+dkumar1402@users.noreply.github.com>
2023-06-09 07:34:02 +0600

    Add files via upload

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,34 @@
+import { useContext, useEffect, useState } from "react";
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+import Home from "./pages/home";
+import ProductsContextProvider from "./context/productsContextProvider";
+import CartContextProvider from "./context/cartContextProvider";
+import Navbar from "./components/navbar";
+import Cart from "./pages/cart";
+import NotFound from "./pages/notFound";
+import ProductDetails from "./pages/productDetails";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ProductsContextProvider>
+        <CartContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </CartContextProvider>
+      </ProductsContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ ecommerce/src/components/product/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./product.css";
+import { Avatar, Card } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import { starRating } from "../../utils/starRating";
+
+function Product({ product, productLayout }) {
+  return (
+    <div className={`product ${productLayout == "list" ? "list__view" : ""} `}>
+      <Link to={`/product/${product.id}`}>
+        <img src={product.images[0]} className="product__image" alt="" />
+        <div className="product__body">
+          <div className="product__title" title={product.title}>
+            {product.title}
+          </div>
+
+          <div className="product__footer">
+            <div className="product__price">&#8377;{product.price}</div>
+            <div className="product__rating">
+              {starRating(product.rating).map((star, i) => (
+                <img key={i} src={star} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Product;
